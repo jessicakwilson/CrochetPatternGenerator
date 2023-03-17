038760afb898f553713d5311f4e6ab57cdc1a5c8
Jessica Wilson <jkw0329@hotmail.com>
2023-03-17 11:46:43 -0400

    initial commit

---
+++ crochet-pattern-generator/pages/index.tsx
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+// import styles from '@/styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Crochet Pattern Generator</title>
+        <meta name="description" content="Crochet Pattern Generator" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <div>
+          <p>
+            Hello World
+          </p>
+        </div>
+      </main>
+    </>
+  )
+}
